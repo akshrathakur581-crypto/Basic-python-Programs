52b0eaf8c5c0fd855103fead2da417465a4735e4
Akshra Thakur <akshrathakur581@gmail.com>
2025-11-29 16:59:01 +0530

    Add program to find the largest of three numbers

---
+++ largest_of_three.py
@@ -0,0 +1,12 @@
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+if a >= b and a >= c:
+    print(a, "is the largest")
+elif b >= a and b >= c:
+    print(b, "is the largest")
+else:
+    print(c, "is the largest")
